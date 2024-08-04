8a20950c26dbe7c4518cd91db33b790ba4d48e01
SoulRiaper <soulriaper@gmail.com>
2024-08-04 11:42:58 +0300

    add json array support

--- src/Loader.ts
+++ src/Loader.ts
@@ -16,36 +16,41 @@ export class Loader {
 
     async loadJsonFilesRecursively(): Promise<{ action: IAction[], rule: IRule[], codelet: ICodelet[] }> {
         const directory = this.watchDir;
-        type JsonObject = IAction | IRule | ICodelet;
+        type JsonObject = [IAction | IRule | ICodelet];
         const objectsByType: { action: IAction[], rule: IRule[], codelet: ICodelet[] } = { action: [], rule: [], codelet: [] };
-    
+
         async function loadJsonFromDirectory(dirPath: string): Promise<void> {
             const entries = await fs.promises.readdir(dirPath, { withFileTypes: true });
-    
+
             for (const entry of entries) {
                 const fullPath = path.join(dirPath, entry.name);
-    
+
                 if (entry.isFile() && entry.name.endsWith('.json')) {
                     const rawData = await fs.promises.readFile(fullPath, 'utf-8');
-                    const data: JsonObject = JSON.parse(rawData);
-    
-                    switch (data.type) {
-                        case 'action':
-                            objectsByType.action.push(data);
-                            break;
-                        case 'rule':
-                            objectsByType.rule.push(data);
-                            break;
-                        case 'codelet':
-                            objectsByType.codelet.push(data);
-                            break;
+                    let data: JsonObject = JSON.parse(rawData);
+                    if (!Array.isArray(data)) {
+                        data = [data];
                     }
+                    for (const item of data) {
+                        switch (item.type) {
+                            case 'action':
+                                objectsByType.action.push(item);
+                                break;
+                            case 'rule':
+                                objectsByType.rule.push(item);
+                                break;
+                            case 'codelet':
+                                objectsByType.codelet.push(item);
+                                break;
+                        }
+                    }
+
                 } else if (entry.isDirectory()) {
                     await loadJsonFromDirectory(fullPath);
                 }
             }
         }
-    
+
         await loadJsonFromDirectory(directory);
         return objectsByType;
     }
